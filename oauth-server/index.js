a7606d580bf709aa28d9c32e9b3043f75c6a5a57
OmkarG <omkargample@gmail.com>
2025-03-31 20:19:27 +0530

    Google auth added

--- oauth-server/index.js
+++ oauth-server/index.js
@@ -53,6 +53,43 @@ app.get("/auth/github/callback", async (req, res) => {
   }
 });
 
+app.get("/auth/google", (req, res) => {
+  const googleAuthUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=http://localhost:${PORT}/auth/google/callback&response_type=code&scope=profile email`;
+  res.redirect(googleAuthUrl);
+});
+
+app.get("/auth/google/callback", async (req, res) => {
+  const { code } = req.query;
+  if (!code) {
+    return res.status(404).json({ error: "Auth code not provided" });
+  }
+  try {
+    const tokenResponse = await axios.post(
+      `https://oauth2.googleapis.com/token`,
+      {
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        client_secret: process.env.GOOGLE_SECRET_KEY,
+        code,
+        grant_type: "authorization_code",
+        redirect_uri: `http://localhost:${PORT}/auth/google/callback`,
+      },
+      {
+        headers: {
+          "Content-Type": "application:x-www-form-urlencoded",
+        },
+      }
+    );
+
+    const accessToken = tokenResponse.data.access_token;
+    console.log(accessToken);
+
+    res.cookie("access_token", accessToken);
+    return res.redirect(`${process.env.FRONTEND_URL}/v1/profile/google`);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
 app.listen(PORT, () => {
   console.log("Server is running on " + PORT);
 });
